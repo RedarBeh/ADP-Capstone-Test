05b41d7ade3cdb37f2cafe75237564f09cdbfa8f
potatogallery <khazsaunders@hotmail.com>
2022-10-22 20:35:34 +0200

    AnnotationService

--- Group19CarWash/Group19CarWash/src/main/java/za/ac/cput/Group19CarWash/services/CarService.java
+++ Group19CarWash/Group19CarWash/src/main/java/za/ac/cput/Group19CarWash/services/CarService.java
@@ -1,12 +1,13 @@
 package za.ac.cput.Group19CarWash.services;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 import za.ac.cput.Group19CarWash.domain.Car;
 import za.ac.cput.Group19CarWash.repository.car.ICarRepo;
 
 import java.util.List;
 import java.util.UUID;
-
+@Service
 public class CarService {
 
     public static ICarRepo iCarRepo;
